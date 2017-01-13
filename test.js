4ee45efec31c1f536971d2de59d54317f4ec626d
Nick The Sick <nperez0111@gmail.com>
2017-01-12 23:59:30 -0500

    added tests

--- test.js
+++ test.js
@@ -1,6 +1,13 @@
 import test from 'ava';
 import fn from './';
 
-test( 'title', t => {
-    t.deepEqual( "2", "2" )
-} );
+test('returns json objects', t => {
+    t.deepEqual((typeof fn('Miami, FL')), (typeof {}))
+    t.deepEqual((typeof fn(22.7, -80.1)), (typeof {}))
+
+
+});
+test('returns promise object', t => {
+    t.true((fn('Miami, FL') instanceof Promise))
+    t.true((fn(22.7, -80.1) instanceof Promise))
+})
