b1a16a644aab6d907ac938cf1d8f96edaf80eb70
Nick The Sick <nperez0111@gmail.com>
2017-01-12 23:45:04 -0500

    Fixes #1 PR

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 'use strict';
-let fetch = require( 'isomorphic-fetch' ),
-    toCoord = require( 'city-to-coords' );
-module.exports = ( city, long ) => {
-    let str = "http://forecast.weather.gov/MapClick.php?FcstType=json&";
+let fetch = require('isomorphic-fetch'),
+    toCoord = require('city-to-coords');
 
-    if ( long === Number( long ) && city === Number( city ) ) {
+function toWeather(city, long) {
+    let str = "http://forecast.weather.gov/MapClick.php?FcstType=json";
 
-        return fetch( str + `lat=${city}&lon=${long}` )
+    if (city !== Number(city)) {
 
-    } else if ( city !== Number( city ) ) {
+        return toCoord(city).then(loc => toWeather(Number(loc.lat), Number(loc.lng)))
 
-        return toCoord( city ).then( a => {
+    } else if (long === Number(long) && city === Number(city)) {
 
-            return fetch( str + `lat=${a.lat}&lon=${a.lng}` ).then( response => {
+        return fetch(`${str}&lat=${city}&lon=${long}`).then(response => {
 
-                if ( response.status >= 400 ) {
+            if (response.status >= 400) {
 
-                    throw new Error( 'Bad response from server' )
+                throw new Error('Bad response from server')
 
-                }
+            }
 
-                return response.json()
+            return response.json()
 
-            } )
+        })
 
-        } )
+
 
     }
 
     return false;
-};
+}
+
+module.exports = toWeather
